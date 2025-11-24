9e19207e16403646f7ae17d0785db068d9e8ffd3
KnoxxLearning <chirwanokuthula@gmail.com>
2025-11-24 04:20:39 +0200

    Here I'm committing the changes I made my 'basic_operations.py' where I was exploring the basics of Python Operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+Sum = number1 + number2
+Difference = number1 - number2
+Product = number1 * number2
+
+print("'Sum' of [number1] and [number2] is", Sum)
+print("'Difference' of [number1] and [number2] is", Difference)
+print("'Product' of number1 and number2 is", Product)
