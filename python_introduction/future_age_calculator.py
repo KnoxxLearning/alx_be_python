52110955352f73a41171deb8399a56753ad0a674
KnoxxLearning <chirwanokuthula@gmail.com>
2025-11-24 13:29:05 +0200

    The 'future_age_calculator.py' file has been updated with the calculation for a user's future age.

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,11 @@
+current_age = input("How old are you? ")
+
+#how do I program this variable to welcome integers as a response?
+
+current_age_int = int(current_age)
+
+future_age = current_age_int + 27
+
+print(future_age)
+
+
