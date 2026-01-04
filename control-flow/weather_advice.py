5142095a982065dbd9e2b2b94d23fc3b83a7f070
KnoxxLearning <chirwanokuthula@gmail.com>
2026-01-04 16:00:06 +0200

    The variable has been changed to Weather Input for the checks

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,12 +1,12 @@
-weather = str(input(" What's the weather like today? (sunny/rainy/cold): "))
+Weather_Input = str(input(" What's the weather like today? (sunny/rainy/cold): "))
 
-if weather == str("sunny"):
+if Weather_Input == str("sunny"):
     print(" Wear a t-shirt and sunglasses.")
     
-elif weather == str("rainy"):
+elif Weather_Input == str("rainy"):
     print("Don't forget your umbrella and a raincoat.")
 
-elif weather == str("cold"):
+elif Weather_Input == str("cold"):
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print(" Sorry, I don't have recommendations for this weather.")
